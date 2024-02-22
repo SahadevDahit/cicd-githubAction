5f5690776f699ae9449c080c11146acffbbbf137
SahadevDahit <sahadevdahit123@gmail.com>
2024-02-22 16:11:31 +0545

    cicd changes

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! Welcome ............'); // Send a response to the client
+    res.send('Hello, World! Welcome to the cicd pipeline'); // Send a response to the client
+});
+app.get('/products', (req, res) => {
+    // Assuming products are stored in an array called 'products'
+    const products = [{
+            id: 1,
+            name: 'Product 1',
+            price: 10
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            price: 20
+        },
+        {
+            id: 3,
+            name: 'Product 3',
+            price: 30
+        }
+    ];
+
+    // Send the products as a JSON response to the client
+    res.json(products);
 });
 
 const port = 4000;
